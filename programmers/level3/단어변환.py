610eb069d375ae88e50eba5d0e4d792127aeff02
s2choco <kyj_0129@naver.com>
2020-05-28 14:15:37 +0900

    programmers level3 2문제 업데이트

--- programmers/level3/단어변환.py
+++ programmers/level3/단어변환.py
@@ -1,50 +1,46 @@
-# -*- coding: utf-8 -*-
-import queue
+import collections
 
 def solution(begin, target, words):
-    graph = [[0]*(len(words)+1) for _ in range(len(words)+1)]
+    if target not in words: return 0
 
-    # begin 인접행렬 그래프 만들기
-    for i in range(len(words)):
-        if diff_one(begin, words[i]):
-            graph[0][i+1] = 1
-            graph[i+1][0] = 1
+    graph = {}
 
-    # words 인접행렬 그래프 만들기
-    for i in range(len(words)):
-        for j in range(i+1, len(words)):
-            if diff_one(words[i], words[j]):
-                graph[i+1][j+1] = 1
-                graph[j+1][i+1] = 1
+    # begin 인접 리스트 만들기
+    for w in words:
+        if changable(begin, w):
+            graph[begin] = graph.get(begin, []) + [w]
 
-    return bfs(graph, target, words)
+    # words 인접 리스트 만들기
+    n = len(words)
+    for i in range(n):
+        for j in range(i, n):
+            if changable(words[i], words[j]):
+                graph[words[i]] = graph.get(words[i], []) + [words[j]]
+                graph[words[j]] = graph.get(words[j], []) + [words[i]]
 
-def bfs(graph, target, words):
-    q = queue.Queue()
-    checked = [0] * (len(words)+1)
-    q.put([0, 0])
-    checked[0] = 1
+    return bfs(begin, target, graph)
 
-    while not q.empty():
-        index, depth = q.get()
-        for i in range(len(graph[index])):
-            if graph[index][i] == 1 and checked[i] == 0:
-                if words[i-1]==target: return depth+1
-                q.put([i, depth+1])
-                checked[i] = 1
-    
-    return 0
+def changable(target, source):
+    diff = 0
+    for i in range(len(target)):
+        if target[i] != source[i]: diff += 1
+    return diff == 1
 
-def diff_one(word1, word2):
-    diff_cnt = 0
+def bfs(v, target, graph):
+    queue = collections.deque()
+    queue.append([v, 0])
+    visited = set()
 
-    for i in range(len(word1)):
-        if word1[i] != word2[i]: 
-            diff_cnt += 1
-    
-    if diff_cnt == 1: return True
-    else: return False
-        
+    while queue:
+        a, depth = queue.popleft()
+
+        if a in visited: continue
+        if a == target: return depth
+
+        visited.add(a)
+
+        if graph.get(a):
+            [queue.append([i, depth+1]) for i in sorted(graph[a])]
 
 if __name__ == "__main__":
     print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
--- programmers/level3/등굣길.py
+++ programmers/level3/등굣길.py
@@ -1,29 +1,25 @@
-# -*- coding: utf-8 -*-
-
 def solution(m, n, puddles):
-    road = [[-1]*m for _ in range(n)]
-    road[0][0] = 1
-    
-    for puddle in puddles:
-        road[puddle[1]-1][puddle[0]-1] = 0
+    roads = [[1]*m for _ in range(n)]
 
-    # 1행 채우기
-    for j in range(1, m):
-        if road[0][j] == -1:
-            road[0][j] = road[0][j-1]
+    for p in puddles:
+        roads[p[1]-1][p[0]-1] = 0
 
     # 1열 채우기
     for i in range(1, n):
-        if road[i][0] == -1:
-            road[i][0] = road[i-1][0]
+        if roads[i-1][0] == 0:
+            roads[i][0] = 0
+
+    # 1행 채우기
+    for j in range(1, m):
+        if roads[0][j-1] == 0:
+            roads[0][j] = 0
 
     for i in range(1, n):
         for j in range(1, m):
-            if road[i][j] == -1:
-                road[i][j] = (road[i-1][j] + road[i][j-1])%1000000007
+            if roads[i][j] == 1:
+                roads[i][j] = (roads[i-1][j] + roads[i][j-1]) % 1000000007
 
-    return road[n-1][m-1]
-
+    return roads[n-1][m-1]
 
 if __name__ == "__main__":
-    print(solution(4,3,[[2,2],[3,2]]))
+    print(solution(4,3,[[2,2]]))

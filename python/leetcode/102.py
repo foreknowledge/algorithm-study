517de9a26abff22858345b02e625ee341fa57228
foreknowledge <kyj_0129@naver.com>
2023-04-26 18:53:28 +0900

    leetcode 102 (solution 3)

--- python/leetcode/102.py
+++ python/leetcode/102.py
@@ -49,5 +49,30 @@ class Solution2:
         return ans
 
 
-print(Solution2().levelOrder(str_array_to_tree('[3,9,20,null,null,15,7]')))
-print(Solution2().levelOrder(str_array_to_tree('[3,9,20,2,5,15,7]')))
+class Solution3:
+    # 2중 루프
+    # 한 depth 마다 루프 돌면서 sub_array 생성 & children 큐에 추가
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        answer = []
+        q = deque([root])
+        while q:
+            size = len(q)
+            arr = []
+            for _ in range(0, size):
+                n = q.popleft()
+                arr.append(n.val)
+                if n.left:
+                    q.append(n.left)
+                if n.right:
+                    q.append(n.right)
+
+            answer.append(arr)
+
+        return answer
+
+
+print(Solution3().levelOrder(str_array_to_tree('[3,9,20,null,null,15,7]')))
+print(Solution3().levelOrder(str_array_to_tree('[3,9,20,2,5,15,7]')))
